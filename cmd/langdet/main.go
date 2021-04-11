550e2caaa615ff14ed9fc3b094dcfc2574e7148e
Marco Nicola <marconicola@disroot.org>
2021-04-11 18:26:23 +0200

    Create langdet main command

---
+++ cmd/langdet/main.go
@@ -0,0 +1,14 @@
+// Copyright (c) 2021, The NLP Odyssey Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/nlpodyssey/langdet/pkg/cli"
+	"os"
+)
+
+func main() {
+	cli.Run(os.Args)
+}
