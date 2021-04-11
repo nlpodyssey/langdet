2f593b43a60341f8abfb3cec1790f842352fcf78
Marco Nicola <marconicola@disroot.org>
2021-04-11 18:42:30 +0200

    Add code to run server (TLS and insecure)

---
+++ pkg/server/run.go
@@ -0,0 +1,121 @@
+// Copyright (c) 2021, The NLP Odyssey Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package server
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/nlpodyssey/langdet/pkg/api"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+	"google.golang.org/grpc"
+	"net"
+	"net/http"
+	"strings"
+)
+
+// Run runs the server according to its configuration.
+func (s *Server) Run() error {
+	if s.config.TLSEnabled {
+		return s.runTLS()
+	}
+	return s.runInsecure()
+}
+
+func (s *Server) address() string {
+	return fmt.Sprintf("%s:%d", s.config.Host, s.config.Port)
+}
+
+func (s *Server) runInsecure() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	grpcServer := grpc.NewServer()
+	api.RegisterApiServer(grpcServer, s)
+
+	gwmux := runtime.NewServeMux()
+	err := api.RegisterApiHandlerServer(ctx, gwmux, s)
+	if err != nil {
+		return fmt.Errorf("failed to register service handler: %w", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", gwmux)
+
+	lis, err := net.Listen("tcp", s.address())
+	if err != nil {
+		return fmt.Errorf("TCP listen error: %w", err)
+	}
+
+	h2s := &http2.Server{}
+	h1s := &http.Server{
+		Handler: h2c.NewHandler(handlerFunc(grpcServer, mux), h2s),
+	}
+
+	s.logger.Info().Msgf("Serving on %s (insecure)", s.address())
+	err = h1s.Serve(lis)
+	if err != nil {
+		return fmt.Errorf("server error: %w", err)
+	}
+	return nil
+}
+
+func (s *Server) runTLS() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	tlsCert, err := tls.LoadX509KeyPair(s.config.TLSCert, s.config.TLSKey)
+	if err != nil {
+		return fmt.Errorf("error loading TLS certificate: %w", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	api.RegisterApiServer(grpcServer, s)
+
+	gwmux := runtime.NewServeMux()
+	err = api.RegisterApiHandlerServer(ctx, gwmux, s)
+	if err != nil {
+		return fmt.Errorf("failed to register service handler: %w", err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", gwmux)
+
+	lis, err := net.Listen("tcp", s.address())
+	if err != nil {
+		return fmt.Errorf("TCP listen error: %w", err)
+	}
+
+	hs := &http.Server{
+		Handler: handlerFunc(grpcServer, mux),
+		TLSConfig: &tls.Config{
+			Certificates: []tls.Certificate{tlsCert},
+			NextProtos:   []string{"h2"},
+		},
+	}
+
+	s.logger.Info().Msgf("Serving on %s (TLS)", s.address())
+	err = hs.Serve(tls.NewListener(lis, hs.TLSConfig))
+	if err != nil {
+		return fmt.Errorf("server error: %w", err)
+	}
+	return nil
+}
+
+func handlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isGRPCRequest(r) {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func isGRPCRequest(r *http.Request) bool {
+	return r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc")
+}
